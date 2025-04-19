ee8b1fe7b8e72c28f92520fae8ff7f06247af6ea
GGital <sakukiheng@gmail.com>
2025-04-19 16:39:50 +0700

    Made array for User Defined Data

---
+++ include/UserCollections.h
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+#include <Task.h>
+using namespace std;
+
+class UserCollections
+{
+private:
+    int userId;
+    string username;
+    string password;
+    Task *tasks;
+    string *categories;
+    int categoryCount;
+    int taskCount;
+
+public:
+    UserCollections(int userId, string u, string p) : userId(userId) username(u), password(p), tasks(nullptr), categories(nullptr), categoryCount(0), taskCount(0)
+    {
+        tasks = new Task[0];        // Initialize tasks to an empty array
+        categories = new string[0]; // Initialize categories to an empty array
+        ReadTasksFromFile();        // Read tasks from file
+        ReadCategoriesFromFile();   // Read categories from file
+    }
+
+    // Destructor to clean up dynamically allocated memory
+    ~UserCollections()
+    {
+        delete[] tasks;
+        delete[] categories;
+    }
+
+    ReadTasksFromFile();
+    ReadCategoriesFromFile();
+    AddTask(Task task);
+};
